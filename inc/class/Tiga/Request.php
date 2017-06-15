451d0ca047b7e930bda6f8f01f7b1c8d6b8c4585
Gama <gama@tonjoo.com>
2017-06-15 11:05:12 +0700

    Update Files bag. Support get/check array input.

--- inc/class/Tiga/Request.php
+++ inc/class/Tiga/Request.php
@@ -58,12 +58,19 @@ class Request {
 	 * @return mixed
 	 */
 	public function input($key, $default = false) {
-		if (isset($this->input[$key])) {
-			return $this->input[$key];
+		if (!preg_match('/(^\w+|(?<!^)\G)\[(.*?)\]/', $key, $matches)) {
+			if (isset($this->input[$key])) {
+				return $this->input[$key];
+			}
+		} else {
+			if ($matches[0] == $key && isset($matches[1]) && isset($matches[2])) {
+				if (isset($this->input[$matches[1]][$matches[2]])) {
+					return $this->input[$matches[1]][$matches[2]];
+				}
+			}
 		}
 
-		if($default)
-			return $default;
+		if ($default) return $default;
 
 		return false;
 	}
@@ -74,11 +81,18 @@ class Request {
 	 * @param mixed 	$default 	
 	 * @return mixed
 	 */
-	public function file($key) {
-		if (isset($this->file[$key])) {
+	public function file($key, $get = '') {
+		if ($this->hasFile($key)) {
+			if (isset($this->file[$key][$get])) { // name, type, tmp_name, error, size
+				return $this->file[$key][$get];
+			} else {
+				if ($get == 'base64') {
+					$data = file_get_contents($this->file[$key]['tmp_name']);
+					return 'data:' . $this->file[$key]['type'] . ';base64,' . base64_encode($data);
+				}
+			}
 			return $this->file[$key];
 		}
-
 		return false;
 	}
 
@@ -90,8 +104,22 @@ class Request {
 	public function has($key) {
 		$keys = explode('|', $key);
 		foreach ($keys as $k) {
-			if (!array_key_exists($k, $this->input) || empty($this->input[$k])) {
-				return false;
+			if (!preg_match('/(^\w+|(?<!^)\G)\[(.*?)\]/', $k, $matches)) {
+				if (!array_key_exists($k, $this->input) || empty($this->input[$k])) {
+					return false;
+				}
+			} else {
+				if ($matches[0] == $k && isset($matches[1]) && isset($matches[2])) {
+					if (!array_key_exists($matches[1], $this->input) || empty($this->input[$matches[1]])) {
+						return false;
+					} else {
+						if (!is_array($this->input[$matches[1]]) || !array_key_exists($matches[2], $this->input[$matches[1]]) || empty($this->input[$matches[1]][$matches[2]])) {
+							return false;
+						}	
+					}
+				} else {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -105,7 +133,7 @@ class Request {
 	public function hasFile($key) {
 		$keys = explode('|', $key);
 		foreach ($keys as $k) {
-			if (!array_key_exists($k, $this->file) || empty($this->file[$k])) {
+			if (!array_key_exists($k, $this->file) || empty($this->file[$k]) || $this->file[$k]['size'] == 0) {
 				return false;
 			}
 		}
